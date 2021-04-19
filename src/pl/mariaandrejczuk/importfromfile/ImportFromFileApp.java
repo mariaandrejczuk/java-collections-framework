ecf409ca4ef7db49d37099332a7ed64dc50aa10f
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-04-19 08:54:08 +0200

    #3 implementacja zad wczytywanie danych, HashSet, Arrays.asList()

---
+++ src/pl/mariaandrejczuk/importfromfile/ImportFromFileApp.java
@@ -0,0 +1,14 @@
+package pl.mariaandrejczuk.importfromfile;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ImportFromFileApp {
+    public static void main(String[] args) {
+        File file = new File();
+        Set<Person> people = new HashSet<>(Arrays.asList(file.getPeople()));
+        System.out.println(people);
+    }
+}
+
